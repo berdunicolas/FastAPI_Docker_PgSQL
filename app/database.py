e9192cb9dec3b0b8a6140efcd1d6116e2518a59a
Nicolas <berdunicolas@gmail.com>
2024-10-17 20:05:35 -0300

    update docker-compose

--- app/database.py
+++ app/database.py
@@ -1,7 +1,8 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:octavio@localhost:5432/fastapi_docker_pgsql"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:octavio@localhost:5432/fastapi_docker_pgsql")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
